1aa116b7c472f6ae3de73337bc02f7ffc1883851
Raixxar <raixxar@gmail.com>
2026-08-06 22:31:46 +0200

    origin/dev/release-v1.0.4

--- Contents/mods/B42 PZLinux/42/media/lua/client/Context/World/ISContextLinuxMenu.lua
+++ Contents/mods/B42 PZLinux/42/media/lua/client/Context/World/ISContextLinuxMenu.lua
@@ -2,7 +2,7 @@ linuxUI = ISPanel:derive("linuxUI")
 
 local STAY_CONNECTED_TIME = 0
 local CONNECTED_TO_INTERNET_TIME = 0
-local PZLinuxVersion = "v1.0.3"
+local PZLinuxVersion = "v1.0.4"
 
 -- CONSTRUCTOR
 function linuxUI:new(x, y, width, height, player)
